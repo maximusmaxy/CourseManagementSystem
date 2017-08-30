658ac21c411ef476de9328b533981fa24375f453
Lisa <lisajcunningham@me.com>
2017-08-30 10:43:11 +1000

    changed department label to area of study

--- CourseManagementSystem/CourseManagementSystem/UnitForm.designer.cs
+++ CourseManagementSystem/CourseManagementSystem/UnitForm.designer.cs
@@ -530,11 +530,11 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(80, 268);
+            this.label7.Location = new System.Drawing.Point(61, 274);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 21);
+            this.label7.Size = new System.Drawing.Size(119, 21);
             this.label7.TabIndex = 105;
-            this.label7.Text = "Department:";
+            this.label7.Text = "Area of Study:";
             // 
             // UnitForm
             // 

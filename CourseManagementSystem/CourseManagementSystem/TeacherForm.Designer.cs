bcd59058cc9ded75369cdf51e068209cc5e6e653
Lisa <lisajcunningham@me.com>
2017-08-30 10:14:46 +1000

    form length adjusted to remove dead space

--- CourseManagementSystem/CourseManagementSystem/TeacherForm.Designer.cs
+++ CourseManagementSystem/CourseManagementSystem/TeacherForm.Designer.cs
@@ -460,7 +460,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(744, 575);
+            this.ClientSize = new System.Drawing.Size(744, 507);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);

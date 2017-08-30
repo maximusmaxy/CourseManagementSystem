b185e619edefc4047d74e7aeb82125ce9aa2afa2
Lisa <lisajcunningham@me.com>
2017-08-30 10:07:56 +1000

    adjusted form length to remove dead space at bottom of the form

--- CourseManagementSystem/CourseManagementSystem/EnrolmentForm.designer.cs
+++ CourseManagementSystem/CourseManagementSystem/EnrolmentForm.designer.cs
@@ -600,7 +600,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(744, 672);
+            this.ClientSize = new System.Drawing.Size(744, 528);
             this.Controls.Add(this.panel9);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label7);

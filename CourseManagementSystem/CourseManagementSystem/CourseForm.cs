3c3c2ba7edc587c75055a8be88bbb71d59e86f0a
Brendon <brendonbano@live.com>
2017-08-31 11:58:14 +1000

    Ongoign course work

--- CourseManagementSystem/CourseManagementSystem/CourseForm.cs
+++ CourseManagementSystem/CourseManagementSystem/CourseForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CmsLibrary;
 
 namespace CMS
 {
@@ -15,6 +16,59 @@ namespace CMS
         public CourseForm()
         {
             InitializeComponent();
+        }
+
+        private void CourseForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!Validation.Word(txtCourseName) || !Validation.Numeric(txtCourseCost) || !Validation.Radio(pnlDeliveryType) 
+                || !Validation.Date(dtpStart) || !Validation.Date(dtpEnd) || !Validation.Combo(cboxCampus) 
+                || !Validation.Combo(cboxAreaOfStudy) || !Validation.Word(txtCourseDescription)
+                )
+            {
+                MessageBox.Show("Failed to Submit, please try again");
+            }
+
+
+            else
+            {
+                Course newCourse = new Course();
+                newCourse.Name = txtCourseName.Text;
+                newCourse.Cost = double.Parse(txtCourseCost.Text);
+                newCourse.DeliveryType = Forms.RadioValue(pnlDeliveryType, Types.DeliveryType);
+                newCourse.StartDate = dtpStart.Value;
+                newCourse.EndDate = dtpEnd.Value;
+                newCourse.LocationId = int.Parse(cboxCampus.SelectedItem.ToString());
+                newCourse.AreaOfStudy = cboxAreaOfStudy.SelectedItem.ToString();
+                newCourse.Description = txtCourseDescription.Text;
+
+
+                newCourse.Add();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

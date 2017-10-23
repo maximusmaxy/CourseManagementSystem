ea8664a5714139a520e858f5167731c2a33d9bcf
maximsumaxy <maxwelllittlejohn@gmail.com>
2017-10-23 14:26:54 +1100

    Added VB Interoperability

---
+++ CourseManagementSystem/VBProject/VBClass.vb
@@ -0,0 +1,14 @@
+﻿Public Class VBClass
+    Public Shared Sub ShowCredits()
+        MessageBox.Show("This program was created by TAFE CULT.  
+Copyright (c) .NET Foundation. All rights reserved. 
+
+Contributors
+--------------
+-Brendon Bano
+-Lisa Cunningham
+-Simon Cunningham
+-Liam Gornall
+-Maxwell LittleJohn")
+    End Sub
+End Class
